248c06fe1a0b82af6ce84ef124ae6ce18397e8d6
Todd Minehardt <todd.minehardt@gmail.com>
2015-05-15 17:44:03 -0500

    Remove this directory.

--- unicycle/etc/.svn/text-base/bash_aliases-unicycletrading.sh.svn-base
+++
@@ -1,4 +0,0 @@
-alias h='history'
-alias ls='ls --color=auto -F'
-alias lsm='ls -altr | more'
-alias rm='rm -i'
--- unicycle/tools/optimize_mysql_tables.sql
+++
@@ -1,3 +0,0 @@
-SELECT concat("OPTIMIZE TABLE ", table_schema, ".", table_name, ";") FROM
-information_schema.tables WHERE DATA_FREE > 0 INTO OUTFILE '/tmp/optimize.sql';
-SOURCE /tmp/optimize.sql;
